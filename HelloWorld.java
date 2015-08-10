e33f22e42158c588e914bcb47f2547d3496ae061
Jack Potato Le <nanotek2711@yahoo.com>
2015-08-10 00:15:37 -0400

    woooo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
-	public static void main(String[] args) {[A[B
+	public static void main(String[] args) {
 		System.out.println("you might think this will print \"Hello World\" well..... you're dead wrong");
 	}
 }
