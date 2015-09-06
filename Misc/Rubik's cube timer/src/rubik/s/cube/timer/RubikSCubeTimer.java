b542454d076c93ba2e3f0b78c5f31b0258bce5a7
Jack Potato Le <nanotek2711@yahoo.com>
2015-09-06 15:58:06 -0400

    remove

--- Misc/Rubik's cube timer/src/rubik/s/cube/timer/RubikSCubeTimer.java
+++
@@ -1,8 +0,0 @@
-package rubik.s.cube.timer;
-public class RubikSCubeTimer {
-    public static void main(String[] args) {
-        TheTimer time = new TheTimer();
-        time.setLocationRelativeTo(null);
-        time.show();
-    }
-}
--- Misc/Truthie The Truth Table/Variabul.java
+++
@@ -1,9 +0,0 @@
-public class Variabul{
-    char name;
-    int value;
-
-    public Variabul(char n, int val){
-        name = n;
-        value = val;
-    }
-}
