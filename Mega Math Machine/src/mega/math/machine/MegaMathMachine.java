e1a1e4c34322900fbda71e714287da9afae169a6
Jack Potato Le <nanotek2711@yahoo.com>
2015-09-06 15:55:17 -0400

    remove

--- Mega Math Machine/src/mega/math/machine/MegaMathMachine.java
+++
@@ -1,9 +0,0 @@
-package mega.math.machine;
-public class MegaMathMachine {
-    public static void main(String[] args) {
-        MMM mega = new MMM();
-        mega.setTitle("Mega Math Machine");
-        mega.setLocationRelativeTo(null);
-        mega.show();
-    }
-}
--- MineSweeper/src/minesweeper/MineSweeper.java
+++
@@ -1,9 +0,0 @@
-package minesweeper;
-public class MineSweeper {
-    public static void main(String[] args) {
-        Sweep sweep = new Sweep();
-        sweep.setLocationRelativeTo(null);
-        sweep.setExtendedState(6);
-        sweep.show();
-    }
-}
